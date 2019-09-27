8029e180e8481946dc29bcebd1d8f8feda49e8ae
dzordich <dzordich@gmail.com>
2019-09-26 21:29:04 -0400

    created todo item model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ToDoItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField()
+    content = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.get_username} | {self.content[0, 71]}'
+
+    class Meta:
+        ordering = ['-date']
