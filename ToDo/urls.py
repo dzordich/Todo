3a6814737cd2a20f86a9cafc6ff7071c93d1ae8c
dzordich <dzordich@gmail.com>
2019-09-29 16:24:07 -0400

    styled navbar and logo

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from backend.api import ToDoResource
 from frontend.views import index
@@ -22,6 +24,8 @@ todo_resource = ToDoResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('registration.backends.simple.urls')),
     path('api/', include(todo_resource.urls)),
     path('', index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
