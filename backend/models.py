f60bac97cc594131d6b3961781e4d6973f7403c5
dzordich <dzordich@gmail.com>
2019-09-29 21:14:28 -0400

    added board model

--- backend/models.py
+++ backend/models.py
@@ -8,8 +8,18 @@ class Task(models.Model):
     completed = models.BooleanField()
     content = models.TextField()
 
+    board = models.ForeignKey('Board', on_delete=models.SET_NULL, null=True, blank=True)
+
     def __str__(self):
         return f'{self.user.username} | {self.date} | {self.content[:71]}'
 
     class Meta:
         ordering = ['-date']
+
+
+class Board(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
