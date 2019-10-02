d2d97c1c098025916105c6f7f206fec4974090ae
dzordich <dzordich@gmail.com>
2019-10-02 14:04:28 -0400

    created userObjectsOnlyAuthorization for API resources

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,45 @@
 from django.contrib.auth.models import User
 from tastypie import fields
 from tastypie.authorization import Authorization
+from tastypie.exceptions import Unauthorized
 from tastypie.resources import ModelResource
 from backend.models import Task, Board
+
+
+
+class UserObjectsOnlyAuthorization(Authorization):
+    # only allows users to request their own stuff
+    def read_list(self, object_list, bundle):
+        return object_list.filter(user=bundle.request.user)
+
+    def read_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def create_list(self, object_list, bundle):
+        # Assuming they're auto-assigned to ``user``.
+        return object_list
+
+    def create_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def update_list(self, object_list, bundle):
+        allowed = []
+
+        # Since they may not all be saved, iterate over them.
+        for obj in object_list:
+            if obj.user == bundle.request.user:
+                allowed.append(obj)
+
+        return allowed
+
+    def update_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def delete_list(self, object_list, bundle):
+        raise Unauthorized("Sorry, no bulk deletes.")
+
+    def delete_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
 
 
 
@@ -21,7 +58,7 @@ class BoardResource(ModelResource):
 
     class Meta:
         queryset = Board.objects.all()
-        authorization = Authorization()
+        authorization = UserObjectsOnlyAuthorization()
         resource_name = "board"
         always_return_data = True
         filtering = {
